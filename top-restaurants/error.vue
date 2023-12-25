3ff032931c44caa3df6e75ee2ee25698c2d3f81f
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2023-12-25 16:09:02 +0700

    catching the not found error

---
+++ top-restaurants/error.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const props = defineProps({
+  error: Object
+});
+
+// const handleError = () => clearError({redirect:'/'})
+
+</script>
+<template>
+  <div>
+    <Nav />
+    <div class="container">
+      <h1>Page not found</h1>
+      <NuxtLink class="btn" to="/">Go Back</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  text-align: center;
+  margin-top: 5rem;
+}
+
+.btn {
+  background-color: lightgreen;
+  color: black;
+  padding: inherit;
+}
+</style>
