689fd0b6b890223faa0482d98abd1964442f84b3
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2023-12-31 20:54:12 +0700

    update default layouts

--- newPortfolio/layouts/default.vue
+++ newPortfolio/layouts/default.vue
@@ -1,15 +1,20 @@
 <template>
-  <nav>
-    <ul>
-      <li>
-        <NuxtLink to="/">Main</NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/about">About</NuxtLink>
-      </li>
-    </ul>
-  </nav>
-  <div>
+  <div class="container mx-auto max-w-2xl">
+    <header>
+      <div class="">
+        <NuxtLink to="/" class="text-xl font-semibold p2 hover:bg-gray">Bobby Rahmanda</NuxtLink>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <NuxtLink to="/">Main</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/about">About</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
     <slot />
   </div>
 </template>
