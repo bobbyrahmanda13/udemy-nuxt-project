8a7bb828c1fcb6b51b50e993dd0edf6c7cc3e8dd
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2023-12-31 21:58:03 +0700

    add components vue nuxt

---
+++ newPortfolio/components/TheNav.vue
@@ -0,0 +1,12 @@
+<template>
+  <nav class="font-mono">
+    <ul class="flex space-x-4">
+      <li>
+        <NuxtLink to="/" class="p1 hover:bg-gray-200">Main</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/about">About</NuxtLink>
+      </li>
+    </ul>
+  </nav>
+</template>
--- newPortfolio/layouts/default.vue
+++ newPortfolio/layouts/default.vue
@@ -4,16 +4,7 @@
       <div class="">
         <NuxtLink to="/" class="text-xl font-semibold p2 hover:bg-gray-200">Bobby Rahmanda</NuxtLink>
       </div>
-      <nav class="font-mono">
-        <ul class="flex space-x-4">
-          <li>
-            <NuxtLink to="/">Main</NuxtLink>
-          </li>
-          <li>
-            <NuxtLink to="/about">About</NuxtLink>
-          </li>
-        </ul>
-      </nav>
+      <TheNav />
     </header>
     <main class="p2">
       <slot />
