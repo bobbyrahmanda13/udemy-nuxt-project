fb8721dbdb65c74b66cc5f210fceada4eee9bae4
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2024-01-07 18:44:29 +0700

    added folder types and import manual

--- newPortfolio/components/ProjectList.vue
+++ newPortfolio/components/ProjectList.vue
@@ -1,12 +1,5 @@
 <script setup lang="ts">
-type Repo = {
-  id: number
-  name: string
-  html_url: string
-  description: string
-  stargazers_count: number
-  pushed_at: string
-}
+import type { Repo } from '../types/type'
 
 const { error, pending, data } = await useFetch<Repo[], Error>('https://api.github.com/users/bobbyrahmanda13/repos')
 
---
+++ newPortfolio/types/type.ts
@@ -0,0 +1,8 @@
+export type Repo = {
+  id: number
+  name: string
+  html_url: string
+  description: string
+  stargazers_count: number
+  pushed_at: string
+}
