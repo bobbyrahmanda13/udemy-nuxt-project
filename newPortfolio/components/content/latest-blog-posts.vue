f8c27b18cbc1c5a73ca83d839d249487f0cd8fc5
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2024-03-06 21:02:00 +0700

    chore: update

--- newPortfolio/components/content/latest-blog-posts.vue
+++ newPortfolio/components/content/latest-blog-posts.vue
@@ -1,7 +1,7 @@
 <template>
   <BlogPosts v-slot="{ posts }" :limit="limit">
     <div class="" v-for="post in posts" :key="post.id">
-      <NuxtLink to="post._path">{{ post.title }}</NuxtLink>
+      <NuxtLink :to="post._path">{{ post.title }}</NuxtLink>
     </div>
   </BlogPosts>
 </template>
@@ -12,4 +12,5 @@ defineProps({
     default: null
   }
 });
+
 </script>
