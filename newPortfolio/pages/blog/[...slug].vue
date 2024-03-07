96ca65c6cf40c4fe48e06faf4b515472d2d22e61
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2024-03-07 20:21:15 +0700

    chore: optional problem issue transition and toc

--- newPortfolio/pages/blog/[...slug].vue
+++ newPortfolio/pages/blog/[...slug].vue
@@ -2,6 +2,7 @@
 const activeId = ref<string | undefined>()
 
 onMounted(() => {
+  // let elements: any = []
   const callback = (entries: string | any) => {
     // console.log(entries);
     for (const entry of entries) {
@@ -16,11 +17,14 @@ onMounted(() => {
     root: null,
     threshold: 0.5
   })
-  const elements = document.querySelectorAll('h2,h3')
+  // setTimeout(() => {
+    const elements = document.querySelectorAll('h2,h3')
 
-  for (const element of elements) {
-    observer.observe(element)
-  }
+    for (const element of elements) {
+      observer.observe(element)
+    }
+  // }, 150);
+
   onBeforeUnmount(() => {
     // console.log('onBeforeUnmount');
     for (const element of elements) {
@@ -30,33 +34,35 @@ onMounted(() => {
 })
 
 </script>
+
 <template>
   <div>
-  <article class="prose dark:prose-invert prose-gray max-w-none">
-    <ContentDoc>
-      <template #not-found>
-        <h1>Document not found</h1>
-      </template>
-      <template v-slot="{ doc }">
-        <!-- {{ doc.body?.toc?.links }} -->
-        <!-- {{ doc.title }} -->
-        <div class="grid grid-cols-6 gap-16">
-          <div :class="{ 'col-span-4': doc.toc, 'col-span-6': !doc.toc }">
-            <ContentRenderer :value="doc" />
+    <article class="prose dark:prose-invert prose-gray max-w-none">
+      <ContentDoc>
+        <template #not-found>
+          <h1>Document not found</h1>
+        </template>
+
+        <template v-slot="{ doc }">
+          <!-- {{ doc.body?.toc?.links }} -->
+          <!-- {{ doc.title }} -->
+          <div class="grid grid-cols-6 gap-16">
+            <div :class="{ 'col-span-4': doc.toc, 'col-span-6': !doc.toc }">
+              <ContentRenderer :value="doc" />
+            </div>
+            <div class="col-span-2 not-prose" v-if="doc.toc">
+              <aside class="sticky top-8">
+                <div class="font-semibold mb-2">
+                  Table of Contents
+                </div>
+                <nav>
+                  <TocLinks :links="doc.body?.toc?.links" :active-id="activeId" />
+                </nav>
+              </aside>
+            </div>
           </div>
-          <div class="col-span-2 not-prose" v-if="doc.toc">
-            <aside class="sticky top-8">
-              <div class="font-semibold mb-2">
-                Table of Contents
-              </div>
-              <nav>
-                <TocLinks :links="doc.body?.toc?.links" :active-id="activeId" />
-              </nav>
-            </aside>
-          </div>
-        </div>
-      </template>
-    </ContentDoc>
-  </article>
-</div>
+        </template>
+      </ContentDoc>
+    </article>
+  </div>
 </template>
