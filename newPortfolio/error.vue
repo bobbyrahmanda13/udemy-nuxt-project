15d1ccb05294827590c2b3f08325ce4df8fafd6f
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2024-03-11 12:59:38 +0700

    fix

--- newPortfolio/error.vue
+++ newPortfolio/error.vue
@@ -2,10 +2,10 @@
   <NuxtLayout>
     <section>
       <h1 class="text-4xl mb4 flex justify-center justify-items-center">
-        {{ error?.statusCode }}
+        {{ props.error?.statusCode }}
       </h1>
       <h2 class="text-xl text-gray-500 dark:text-gray-200 flex justify-center justify-items-center">
-        {{ error?.statusMessage }}
+        {{ props.error?.statusMessage }}
       </h2>
       <div class="mt5 flex justify-center">
         <NuxtLink class="text-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" to="/">go back to home</NuxtLink>
