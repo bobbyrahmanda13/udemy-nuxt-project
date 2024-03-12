621ef597853f88df023df205c052ea0b9371066f
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2024-03-12 20:38:51 +0700

    chore: update

--- newPortfolio/error.vue
+++ newPortfolio/error.vue
@@ -8,7 +8,8 @@
         {{ props.error?.statusMessage }}
       </h2>
       <div class="mt5 flex justify-center">
-        <NuxtLink class="text-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" to="/">go back to home</NuxtLink>
+        <NuxtLink class="text-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" to="/">go
+          back to home</NuxtLink>
       </div>
     </section>
   </NuxtLayout>
